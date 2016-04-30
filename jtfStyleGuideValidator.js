441def3c4ad31f3aed868b8e17a38d7d1bc05263
kongou-ae <y.matsumoto.ae@gmail.com>
2016-04-30 22:58:22 +0900

    使い方を追記

--- jtfStyleGuideValidator.js
+++ jtfStyleGuideValidator.js
@@ -1,4 +1,6 @@
 function validateSentence(sentence) {
+    //　使い方はREADMEを参照ください。
+    // https://github.com/kongou-ae/redpen-validator/blob/master/README.md
     /*
     console = {
         log:print
