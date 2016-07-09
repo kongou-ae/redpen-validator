85586041abaf455c6001de1bf6ac5daa314a45c5
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-10 02:33:19 +0900

    modify
    #1

--- test/alphabet-must-be-one-byte/test.js
+++ test/alphabet-must-be-one-byte/test.js
@@ -2,7 +2,7 @@ var expectedResult = 1
 var exec = require('child_process').exec;
 exec('redpen -c ./alphabet-must-be-one-byte/redpen-conf.xml ../alphabet-must-be-one-byte/test.md -r json', function(err, stdout, stderr){
   var result = JSON.parse(stdout)
-  if (result == expectedResult){
+  if (result.length != expectedResult){
     process.exit(1);
   }
 });
