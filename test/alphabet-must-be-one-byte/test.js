3dbfc2e54d70260c48ede9b83a1ed42fa77b4c89
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-10 02:39:09 +0900

    error test
    #1

--- test/alphabet-must-be-one-byte/test.js
+++ test/alphabet-must-be-one-byte/test.js
@@ -1,4 +1,4 @@
-var expectedResult = 1
+var expectedResult = 0
 var exec = require('child_process').exec;
 exec('redpen -c ./alphabet-must-be-one-byte/redpen-conf.xml ../alphabet-must-be-one-byte/test.md -r json', function(err, stdout, stderr){
   var result = JSON.parse(stdout)
