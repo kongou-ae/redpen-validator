1586fefa310632163e004c40414c38cac6bfb8cd
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-10 20:08:28 +0900

    modify the path of configuration
    #1

--- test/alphabet-must-be-one-byte/test.js
+++ test/alphabet-must-be-one-byte/test.js
@@ -1,6 +1,6 @@
 var expectedResult = 1
 var exec = require('child_process').exec;
-exec('redpen -c ./alphabet-must-be-one-byte/redpen-conf.xml ../alphabet-must-be-one-byte/test.md -r json', function(err, stdout, stderr){
+exec('redpen -c ../alphabet-must-be-one-byte/redpen-conf.xml ../alphabet-must-be-one-byte/test.md -r json', function(err, stdout, stderr){
   var result = JSON.parse(stdout)
   if (result[0].errors.length  != expectedResult){
     console.log(result[0].errors.length + " error was occured. It is unexpected")
--- test/ka-is-written-in-hiragana/test.js
+++ test/ka-is-written-in-hiragana/test.js
@@ -1,6 +1,6 @@
 var expectedResult = 24
 var exec = require('child_process').exec;
-exec('redpen -c ./ka-is-written-in-hiragana/redpen-conf.xml ../ka-is-written-in-hiragana/test.md -r json', function(err, stdout, stderr){
+exec('redpen -c ../ka-is-written-in-hiragana/redpen-conf.xml ../ka-is-written-in-hiragana/test.md -r json', function(err, stdout, stderr){
   var result = JSON.parse(stdout)
   if (result[0].errors.length  != expectedResult){
     console.log(result[0].errors.length + " error was occured. It is unexpected")
