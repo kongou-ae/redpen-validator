aff300a3f4f6fcc89462daf89d59567992167469
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-10 21:41:22 +0900

    try...
    #1

--- test/script/alphabet-must-be-one-byte.js
+++ test/script/alphabet-must-be-one-byte.js
@@ -1,7 +1,9 @@
 var expectedResult = 1
 var exec = require('child_process').exec;
-exec('redpen -c ../alphabet-must-be-one-byte/redpen-conf.xml ../alphabet-must-be-one-byte/test.md -r json', function(err, stdout, stderr){
+exec('redpen -c test/redpen-conf.xml test/doc/alphabet-must-be-one-byte.md -r json', function(err, stdout, stderr){
   console.log(stdout)
+  console.log(stderr)
+  console.log(err)
   var result = JSON.parse(stdout)
   if (result[0].errors.length  != expectedResult){
     console.log(result[0].errors.length + " error was occured. It is unexpected")
