93408387d1b9f7d55a71242dcb53d958f259081f
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-10 22:03:45 +0900

    modify
    #1

--- test/script/alphabet-must-be-one-byte.js
+++ test/script/alphabet-must-be-one-byte.js
@@ -1,14 +1,11 @@
 var expectedResult = 1
 var exec = require('child_process').exec;
 exec('redpen -c test/redpen-conf.xml test/doc/alphabet-must-be-one-byte.md -r json', function(err, stdout, stderr){
-  console.log(stdout)
-  console.log(stderr)
-  console.log(err)
   var result = JSON.parse(stdout)
   if (result[0].errors.length  != expectedResult){
-    console.log(result[0].errors.length + " error was occured. It is unexpected")
+    console.log(result[0].errors.length + " error was occured. It is unexpected.")
     process.exit(1);
   } else {
-    console.log(result[0].errors.length + " error was occured")
+    console.log(result[0].errors.length + " error was occured. This is expected.")
   }
 });
