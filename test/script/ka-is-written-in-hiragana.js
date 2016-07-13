6a78fa7d6dcd0fda346fc1e269dc7b5fce729616
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-13 22:32:02 +0900

    Revert "modify"
    This reverts commit fcf39c2b2ea3d777b8eaeec66bb1fb1e14eb1b5e.

--- test/script/ka-is-written-in-hiragana.js
+++ test/script/ka-is-written-in-hiragana.js
@@ -1,21 +1,11 @@
 var expectedResult = 24
 var exec = require('child_process').exec;
-var fs = require('fs');
-
-var r = fs.createReadStream('ka-is-written-in-hiragana.js'),
-    w = fs.createWriteStream('test/ka-is-written-in-hiragana.js');
-
-r.pipe(w);
-w.on('close', function(){
-  exec('redpen -c test/redpen-conf.xml test/doc/ka-is-written-in-hiragana.md -r json', function(err, stdout, stderr){
-    fs.unlink('test/ka-is-written-in-hiragana.js',function(){
-      var result = JSON.parse(stdout)
-      if (result[0].errors.length  != expectedResult){
-        console.log(result[0].errors.length + " error was occured. It is unexpected.")
-        process.exit(1);
-      } else {
-        console.log(result[0].errors.length + " error was occured. This is expected.")
-      }
-    });
-  });
+exec('redpen -c test/redpen-conf.xml test/doc/ka-is-written-in-hiragana.md -r json', function(err, stdout, stderr){
+  var result = JSON.parse(stdout)
+  if (result[0].errors.length  != expectedResult){
+    console.log(result[0].errors.length + " error was occured. It is unexpected.")
+    process.exit(1);
+  } else {
+    console.log(result[0].errors.length + " error was occured. This is expected.")
+  }
 });
--- test/script/katakana-must-be-two-byte.js
+++ test/script/katakana-must-be-two-byte.js
@@ -1,21 +1,11 @@
 var expectedResult = 55
 var exec = require('child_process').exec;
-var fs = require('fs');
-
-var r = fs.createReadStream('katakana-must-be-two-byte.js'),
-    w = fs.createWriteStream('test/katakana-must-be-two-byte.js');
-
-r.pipe(w);
-w.on('close', function(){
-  exec('redpen -c test/redpen-conf.xml test/doc/katakana-must-be-two-byte.md -r json', function(err, stdout, stderr){
-    fs.unlink('test/katakana-must-be-two-byte.js',function(){
-      var result = JSON.parse(stdout)
-      if (result[0].errors.length  != expectedResult){
-        console.log(result[0].errors.length + " error was occured. It is unexpected.")
-        process.exit(1);
-      } else {
-        console.log(result[0].errors.length + " error was occured. This is expected.")
-      }
-    });
-  });
+exec('redpen -c test/redpen-conf.xml test/doc/katakana-must-be-two-byte.md -r json', function(err, stdout, stderr){
+  var result = JSON.parse(stdout)
+  if (result[0].errors.length  != expectedResult){
+    console.log(result[0].errors.length + " error was occured. It is unexpected.")
+    process.exit(1);
+  } else {
+    console.log(result[0].errors.length + " error was occured. This is expected.")
+  }
 });
