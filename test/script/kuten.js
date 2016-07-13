4f632143ac172c8b588f2c060dd57196d1a7227a
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-13 22:38:17 +0900

    mofidy the test code
    #1

--- test/script/kuten.js
+++ test/script/kuten.js
@@ -1,11 +1,21 @@
 var expectedResult = 9
 var exec = require('child_process').exec;
-exec('redpen -c test/redpen-conf.xml test/doc/kuten.md -r json', function(err, stdout, stderr){
-  var result = JSON.parse(stdout)
-  if (result[0].errors.length  != expectedResult){
-    console.log(result[0].errors.length + " error was occured. It is unexpected.")
-    process.exit(1);
-  } else {
-    console.log(result[0].errors.length + " error was occured. This is expected.")
-  }
+var fs = require('fs');
+
+var r = fs.createReadStream('kuten.js'),
+    w = fs.createWriteStream('test/kuten.js');
+
+r.pipe(w);
+w.on('close', function(){
+  exec('redpen -c test/redpen-conf.xml test/doc/kuten.md -r json', function(err, stdout, stderr){
+    fs.unlink('test/kuten.js',function(){
+      var result = JSON.parse(stdout)
+      if (result[0].errors.length  != expectedResult){
+        console.log(result[0].errors.length + " error was occured. It is unexpected.")
+        process.exit(1);
+      } else {
+        console.log(result[0].errors.length + " error was occured. This is expected.")
+      }
+    });
+  });
 });
