54b2fab94e28923041510420916842599bc11122
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-15 21:50:50 +0900

    modify

--- test/script/alphabet-must-be-one-byte.js
+++ test/script/alphabet-must-be-one-byte.js
@@ -8,9 +8,10 @@ var r = fs.createReadStream('alphabet-must-be-one-byte.js'),
 r.pipe(w);
 w.on('close', function(){
   exec('redpen -c test/redpen-conf.xml test/doc/alphabet-must-be-one-byte.md -r json', function(err, stdout, stderr){
+    if (err) throw err;
     fs.unlink('test/alphabet-must-be-one-byte.js',function(){
       var result = JSON.parse(stdout)
-      if (result[0].errors.length  != expectedResult){
+      if (result[0].errors.length !== expectedResult){
         throw new Error(result[0].errors.length + " error was occured. It is unexpected.")
       } else {
         console.log(result[0].errors.length + " error was occured. This is expected.")
--- test/script/symbol.js
+++ test/script/symbol.js
@@ -8,11 +8,11 @@ var r = fs.createReadStream('symbol.js'),
 r.pipe(w);
 w.on('close', function(){
   exec('redpen -c test/redpen-conf.xml test/doc/symbol.md -r json', function(err, stdout, stderr){
+    if (err) throw err;
     fs.unlink('test/symbol.js',function(){
       var result = JSON.parse(stdout)
-      if (result[0].errors.length  != expectedResult){
-        console.log(result[0].errors.length + " error was occured. It is unexpected.")
-        process.exit(1);
+      if (result[0].errors.length !== expectedResult){
+        throw new Error(result[0].errors.length + " error was occured. It is unexpected.")
       } else {
         console.log(result[0].errors.length + " error was occured. This is expected.")
       }
