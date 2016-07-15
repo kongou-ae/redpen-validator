c45fc0d4ca667a6e016c9850d12160d8ec8d9e5e
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-15 21:43:22 +0900

    change process.exit

--- test/script/alphabet-must-be-one-byte.js
+++ test/script/alphabet-must-be-one-byte.js
@@ -11,8 +11,7 @@ w.on('close', function(){
     fs.unlink('test/alphabet-must-be-one-byte.js',function(){
       var result = JSON.parse(stdout)
       if (result[0].errors.length  != expectedResult){
-        console.log(result[0].errors.length + " error was occured. It is unexpected.")
-        process.exit(1);
+        throw new Error(result[0].errors.length + " error was occured. It is unexpected.")
       } else {
         console.log(result[0].errors.length + " error was occured. This is expected.")
       }
