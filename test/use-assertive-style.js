ffc99bdae007dd0b9a95d19c2645d24b2c71042a
kongou-ae <y.matsumoto.ae@gmail.com>
2016-07-22 01:02:52 +0900

    add ではないだろうか　他

--- test/use-assertive-style.js
+++ test/use-assertive-style.js
@@ -84,61 +84,82 @@ function validateSentence(sentence) {
             ]
         },
         {
-            'target':'ではないだろうか',
-        }
+            'target':'ではないだろうか'
+        },
+        {
+            'target':'でしょう'
+        },
+        {
+            'target':'と感じる',
+            'tokens':[
+                {
+                    'tags0':'助詞',
+                    'tags1':'格助詞',
+                    'tags6':'と'
+                },
+                {
+                    'tags0':'動詞',
+                    'tags1':'自立',
+                    'tags6':'感じる'
+                }
+            ]
+        },
     ];
 
-    var validate = function(sentence){
+    var validateRegex = function(sentence,terms){
+        var regex = new RegExp( terms.target );
+        if ( regex.test(sentence) ) {
+            return terms.target;
+        }
+    }
 
-        for (var j = 0; j < terms.length; j++) {
-            for (var i = 0; i < sentence.tokens.length; i++) {
-
-                // Tokenizeが不要なものを先に処理
-                if ( !terms[j].hasOwnProperty('tokens') ){
-                    var regex = new RegExp( terms[j].target );
-                    if ( regex.test(sentence) ) {
-                        return terms[j].target;
-                    } else {
-                        return
-                    }
+    var validateToken = function(sentence,terms){
+        // Tokenを見ていく
+        for (var i = 0; i < sentence.tokens.length; i++) {
+            // 2文字のチェック
+            if ( i + 1 < sentence.tokens.length ){
+                if (
+                    sentence.tokens[i].tags[0] === terms.tokens[0].tags0 &&
+                    sentence.tokens[i].tags[1] === terms.tokens[0].tags1 &&
+                    sentence.tokens[i].tags[6] === terms.tokens[0].tags6 &&
+                    sentence.tokens[i+1].tags[0] === terms.tokens[1].tags0 &&
+                    sentence.tokens[i+1].tags[1] === terms.tokens[1].tags1 &&
+                    sentence.tokens[i+1].tags[6] === terms.tokens[1].tags6
+                ){
+                    return terms.target;
                 }
-
-                // 2文字のチェック
-                if ( i + 1 < sentence.tokens.length ){
-                    if (
-                        sentence.tokens[i].tags[0] === terms[j].tokens[0].tags0 &&
-                        sentence.tokens[i].tags[1] === terms[j].tokens[0].tags1 &&
-                        sentence.tokens[i].tags[6] === terms[j].tokens[0].tags6 &&
-                        sentence.tokens[i+1].tags[0] === terms[j].tokens[1].tags0 &&
-                        sentence.tokens[i+1].tags[1] === terms[j].tokens[1].tags1 &&
-                        sentence.tokens[i+1].tags[6] === terms[j].tokens[1].tags6
-                    ){
-                        return terms[j].target;
-                    }
-                }
-
-                // 3文字のチェック
-                if ( i + 2 < sentence.tokens.length ){
-                    if (
-                        sentence.tokens[i].tags[0] === terms[j].tokens[0].tags0 &&
-                        sentence.tokens[i].tags[1] === terms[j].tokens[0].tags1 &&
-                        sentence.tokens[i].tags[6] === terms[j].tokens[0].tags6 &&
-                        sentence.tokens[i+1].tags[0] === terms[j].tokens[1].tags0 &&
-                        sentence.tokens[i+1].tags[1] === terms[j].tokens[1].tags1 &&
-                        sentence.tokens[i+1].tags[6] === terms[j].tokens[1].tags6 &&
-                        sentence.tokens[i+2].tags[0] === terms[j].tokens[2].tags0 &&
-                        sentence.tokens[i+2].tags[1] === terms[j].tokens[2].tags1 &&
-                        sentence.tokens[i+2].tags[6] === terms[j].tokens[2].tags6
-                    ){
-                        return terms[j].target;
-                    }
+            }
+            // 3文字のチェック
+            if ( i + 2 < sentence.tokens.length ){
+                if (
+                    sentence.tokens[i].tags[0] === terms.tokens[0].tags0 &&
+                    sentence.tokens[i].tags[1] === terms.tokens[0].tags1 &&
+                    sentence.tokens[i].tags[6] === terms.tokens[0].tags6 &&
+                    sentence.tokens[i+1].tags[0] === terms.tokens[1].tags0 &&
+                    sentence.tokens[i+1].tags[1] === terms.tokens[1].tags1 &&
+                    sentence.tokens[i+1].tags[6] === terms.tokens[1].tags6 &&
+                    sentence.tokens[i+2].tags[0] === terms.tokens[2].tags0 &&
+                    sentence.tokens[i+2].tags[1] === terms.tokens[2].tags1 &&
+                    sentence.tokens[i+2].tags[6] === terms.tokens[2].tags6
+                ){
+                    return terms.target;
                 }
             }
         }
     }
 
-    var error = validate(sentence);
-    if ( error ){
-        addError( '「' + error + '」はあいまいな表現です。断定系で表現してください。' , sentence);
+    var error = ''
+    for (var j = 0; j < terms.length; j++) {
+        if ( !terms[j].hasOwnProperty('tokens') ){
+            error = validateRegex(sentence,terms[j]);
+        }
+
+        if ( terms[j].hasOwnProperty('tokens') ){
+            error = validateToken(sentence,terms[j]);
+        }
+
+        if ( error ){
+            addError( '「' + error + '」はあいまいな表現です。断定系で表現してください。' , sentence);
+        }
     }
 }
